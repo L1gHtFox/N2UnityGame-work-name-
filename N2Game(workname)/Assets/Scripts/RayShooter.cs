0986e348132f77148fdc6089a2ed94bfc957e829
L1gHtFox <gff1038m@gmail.com>
2018-10-31 18:45:33 +0300

    Допилил логику, но всё ещё есть баги

--- N2Game(workname)/Assets/Scripts/RayShooter.cs
+++ N2Game(workname)/Assets/Scripts/RayShooter.cs
@@ -24,11 +24,9 @@ public class RayShooter : MonoBehaviour {
 	void Update () {
 		if (Input.GetMouseButtonDown (0)) {
 			Vector3 point = new Vector3 (_camera.pixelWidth / 2, _camera.pixelHeight / 2, 0);
-					if (_crystal == null) {
 						_crystal = Instantiate (CrystalPrefab) as GameObject;
 						_crystal.transform.position = transform.TransformPoint (Vector3.forward * 1.5f);
 						_crystal.transform.rotation = transform.rotation;
-					}
 				}
 		}
 }
