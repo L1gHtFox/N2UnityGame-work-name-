5fe459fad36519127bc6f0d3cf80c204b5144880
L1gHtFox <gff1038m@gmail.com>
2018-11-03 14:19:43 +0300

    Добавил первого противника

---
+++ N2Game(workname)/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour {
+	public GameObject playerCharecter;
+	public float dist;
+	NavMeshAgent nav;
+	public float radius = 10;
+
+	void Start () {
+		nav = GetComponent<NavMeshAgent> ();
+	}
+
+	void Update () {
+		dist = Vector3.Distance (playerCharecter.transform.position, transform.position);
+		if (dist > radius) {
+			nav.enabled = false;
+			gameObject.GetComponent<Animator> ().SetTrigger ("Idle");
+		} 
+		if(dist < radius){
+			nav.enabled = true;
+			nav.SetDestination (playerCharecter.transform.position);
+			gameObject.GetComponent<Animator> ().SetTrigger ("Walk");
+		}
+	}
+}
