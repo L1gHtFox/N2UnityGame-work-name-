f38b74cca6017223cff5b1d3f80e60f77db22413
L1gHtFox <gff1038m@gmail.com>
2018-11-06 22:29:54 +0300

    инвентарь 0.4 теперь иконки подсвечиваются

---
+++ N2Game(workname)/Assets/Scripts/Slot.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Slot : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+
+	public Sprite activeCell;
+
+	Sprite cell;
+	Image img;
+
+	void Start () {
+		img = GetComponent<Image>();
+		cell = img.sprite;
+	}
+	
+
+	public void OnPointerEnter(PointerEventData eventData){
+		img.sprite = activeCell;
+	}
+
+	public void OnPointerExit(PointerEventData eventData){
+		img.sprite = cell;
+	}
+}
