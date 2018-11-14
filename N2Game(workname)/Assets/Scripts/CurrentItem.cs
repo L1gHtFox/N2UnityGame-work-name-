772b3324e73e0b10e67a9086332d5d5bcdfb8497
L1gHtFox <gff1038m@gmail.com>
2018-11-14 18:16:41 +0300

    Revert "Merge branch 'master' of https://github.com/L1gHtFox/N2UnityGame-work-name-"
    This reverts commit 4ab92b2bbebb0ff2187a30d50b970825e7bb1e9e, reversing
    changes made to e6ee259bbb10ed16d508725cae8b2a84c5bb75b2.

--- N2Game(workname)/Assets/Scripts/CurrentItem.cs
+++ N2Game(workname)/Assets/Scripts/CurrentItem.cs
@@ -18,44 +18,16 @@ public class CurrentItem : MonoBehaviour, IPointerClickHandler {
 	}
 		
 	public void OnPointerClick (PointerEventData eventData){
-		if (eventData.button == PointerEventData.InputButton.Left) {
-			if(inventory.item[index].customEvent != null){
-				inventory.item [index].customEvent.Invoke ();
-			}
-			if (inventory.item [index].isRemoveable) {
-				Remove ();
-			}
-		}
 		if (eventData.button == PointerEventData.InputButton.Right) {
-			if (inventory.item [index].isDroped) {
-				Drop ();
-				Remove ();
-			}
-		}
-	}
-
-	void Remove(){
-		if (inventory.item [index].id != 0) {
-			
-			if (inventory.item [index].countItem > 1) {
-				inventory.item [index].countItem--;
-			} else {
-				inventory.item [index] = new Item ();
-			}
-			inventory.DisplayItems ();
-		}
-	}
-
-	void Drop(){
-		if (inventory.item [index].id != 0) {
-			for (int i = 0; i < inventory.database.transform.childCount; i++) {
-				Item item = inventory.database.transform.GetChild (i).GetComponent<Item> ();
-				if (item){
-					if(inventory.item[index].id == item.id){
-						GameObject dropdeObj = Instantiate (item.gameObject);
-						dropdeObj.transform.position = Camera.main.transform.position + Camera.main.transform.forward;
-					}
+			if (inventory.item [index].id != 0) {
+				GameObject dropdeObj = Instantiate (Resources.Load<GameObject> (inventory.item [index].pathPrefab));
+				dropdeObj.transform.position = Camera.main.transform.position + Camera.main.transform.forward;
+				if (inventory.item [index].countItem > 1) {
+					inventory.item [index].countItem--;
+				} else {
+					inventory.item [index] = new Item ();
 				}
+				inventory.DisplayItems ();
 			}
 		}
 	}
