c71951a079894525d5ca6e912b6d8b6584170d88
L1gHtFox <gff1038m@gmail.com>
2018-11-14 18:16:27 +0300

    Revert "Revert "Merge branch 'master' of https://github.com/L1gHtFox/N2UnityGame-work-name-""
    This reverts commit cc32cd7c0396d1673aa740f644e11eef64779cf5.

---
+++ N2Game(workname)/Assets/Scripts/HelthBar.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HelthBar : MonoBehaviour {
+
+	public void Healing(int value){
+		
+		GetComponent<Slider> ().value += value;
+
+	}
+}
