fca917abeb412a8e329ea244da3a6198c82ebdab
L1gHtFox <gff1038m@gmail.com>
2018-11-14 18:29:53 +0300

    Revert "Revert "Revert "Revert "инвентарь 0.5 теперь предметы можно выкидывать из инвентаря,предметы rigitbody(имеют падают и отталкиваются от стен)//Наконец доделал половину по инвентарю""""
    This reverts commit b736baa508cf1870904cd46ebe6857a5c18d088b.

---
+++ N2Game(workname)/Assets/Scripts/CurrentItem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CurrentItem : MonoBehaviour, IPointerClickHandler {
+
+	[HideInInspector]
+	public int index;
+
+	GameObject inventoryobj;
+	Inventory inventory;
+
+
+	void Start () {
+		inventoryobj = GameObject.FindWithTag ("InventoryManager");
+		inventory = inventoryobj.GetComponent<Inventory> ();
+	}
+		
+	public void OnPointerClick (PointerEventData eventData){
+		if (eventData.button == PointerEventData.InputButton.Right) {
+			if (inventory.item [index].id != 0) {
+				GameObject dropdeObj = Instantiate (Resources.Load<GameObject> (inventory.item [index].pathPrefab));
+				dropdeObj.transform.position = Camera.main.transform.position + Camera.main.transform.forward;
+				if (inventory.item [index].countItem > 1) {
+					inventory.item [index].countItem--;
+				} else {
+					inventory.item [index] = new Item ();
+				}
+				inventory.DisplayItems ();
+			}
+		}
+	}
+}
