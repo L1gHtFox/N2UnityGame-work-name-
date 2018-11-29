4ba303964d84da39fa3390a80ef567181b488b01
L1gHtFox <gff1038m@gmail.com>
2018-11-29 18:59:27 +0300

    Перелопатил интерфейс

--- N2Game(workname)/Assets/Scripts/HelthBar.cs
+++ N2Game(workname)/Assets/Scripts/HelthBar.cs
@@ -4,10 +4,38 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class HelthBar : MonoBehaviour {
+	[SerializeField]
+	private Slider HpSlider;
 
-	public void Healing(int value){
+	public float currentHealth = 100;
+
+	private float maxHealth = 100;
+	private float minHealth = 0;
+
+	void Star(){
 		
-		GetComponent<Slider> ().value += value;
+	}
 
+	void Update(){
+		if (Input.GetKeyDown (KeyCode.E)) {
+			PlayerDamage (10);
+		}
+		if(Input.GetKeyDown(KeyCode.R)){
+			PlayerHealing(10);
+		}
+	}
+
+	public void PlayerDamage (float damage){
+		currentHealth -= damage;
+		HpSlider.value = currentHealth;
+		if (currentHealth < 0) {
+			currentHealth = minHealth;
+		}
+	}
+
+	public void PlayerHealing(float healing){
+		
+		currentHealth += healing;
+		HpSlider.value = currentHealth;
 	}
 }
---
+++ N2Game(workname)/Assets/Scripts/N2Bar.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class N2Bar : MonoBehaviour {
+	private Slider slider;
+
+	public float currentN2 = 100;
+	public float time = 1f;
+
+	private float minN2 = 0;
+
+
+
+	void Start () {
+		slider = GetComponent<Slider> ();
+		slider.interactable = false;
+		StartCoroutine (MinusN2 (2));
+	}
+
+	public IEnumerator MinusN2(float n){
+		while(currentN2 >= minN2){
+			yield return new WaitForSeconds(time);
+			currentN2 -= n;
+			slider.value = currentN2;
+		}
+	}
+}
