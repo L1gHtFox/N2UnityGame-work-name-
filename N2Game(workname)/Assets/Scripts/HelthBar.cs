209905ba7ddf4fe28fcc3a8d50586a7bf10cf50d
L1ghtFox <reyanar13@gmail.com>
2018-12-03 14:20:11 +0300

    Revert "Так блэд"
    This reverts commit 7264d9fc4c7ffa92b35304e8650d1018329b3039.

---
+++ N2Game(workname)/Assets/Scripts/HelthBar.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HelthBar : MonoBehaviour {
+	[SerializeField]
+	private Slider HpSlider;
+
+	public float currentHealth = 100;
+
+	private float maxHealth = 100;
+	private float minHealth = 0;
+
+	void Star(){
+		
+	}
+
+	void Update(){
+		if (Input.GetKeyDown (KeyCode.E)) {
+			PlayerDamage (10);
+		}
+		if(Input.GetKeyDown(KeyCode.R)){
+			PlayerHealing(10);
+		}
+	}
+
+	public void PlayerDamage (float damage){
+		currentHealth -= damage;
+		HpSlider.value = currentHealth;
+		if (currentHealth < 0) {
+			currentHealth = minHealth;
+		}
+	}
+
+	public void PlayerHealing(float healing){
+		
+		currentHealth += healing;
+		HpSlider.value = currentHealth;
+	}
+}
