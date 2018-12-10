d479d354bbdfb7fbb62f41abe3acbde5b88b7bb3
L1ghtFox <reyanar13@gmail.com>
2018-12-10 17:05:17 +0300

    Вроде как сделал переход, но прогресс в инвентаре пока не сохраняется

---
+++ N2Game(workname)/Assets/Scripts/LevelManagerThings/DoNotDestroyOnLoad.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoNotDestroyOnLoad : MonoBehaviour {
+
+	private static DoNotDestroyOnLoad instance;
+
+	void Awake(){
+		if (!instance) {
+			instance = this;
+		} else {
+			Destroy (this.gameObject);
+		}
+		DontDestroyOnLoad (this.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
