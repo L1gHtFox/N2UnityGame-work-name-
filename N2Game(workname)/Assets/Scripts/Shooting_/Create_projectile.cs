5a82823eb04d768ff455e53d376e6703fc8e1d45
Danichka <razor375@mail.ru>
2019-01-27 23:35:08 +0300

    Projectile.1.1
    Шарики летят, дамага нет, определения патронов тож

---
+++ N2Game(workname)/Assets/Scripts/Shooting_/Create_projectile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Create_projectile : MonoBehaviour
+{
+    private GameObject _prefabProjectile;
+    private GameObject _camera;
+     
+    void Start()
+    {
+        _prefabProjectile = Resources.Load("projectile") as GameObject;
+        _camera = GameObject.FindWithTag("MainCamera");
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject projectile = Instantiate(_prefabProjectile);
+
+            projectile.transform.position = _camera.transform.TransformPoint(Vector3.forward * 1.5f);
+            projectile.transform.rotation = _camera.transform.rotation;
+        }
+    }
+}
